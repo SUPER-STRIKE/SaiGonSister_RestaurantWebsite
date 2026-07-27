863654808f9ecaf8f100e6ab215110888893ab2a
Anh-Tuan-Hoang9999 <superstrikehsgs@gmail.com>
2026-07-27 09:46:16 -0400

    Listen on Railway PORT and 0.0.0.0 so public networking works.

--- server/server.js
+++ server/server.js
@@ -29,6 +29,6 @@ app.use((req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
